0de859886c3dc4652c12e3c9e644cbb107e5ab06
sapslaj <saps.laj@gmail.com>
2022-01-31 03:02:23 -0500

    tf stuff

--- serverless.tf
+++ serverless.tf
@@ -49,35 +49,3 @@ locals {
     }
   }
 }
-
-resource "local_file" "serverless_config" {
-  content  = jsonencode(local.config)
-  filename = "${path.module}/serverless.json"
-}
-
-resource "null_resource" "make_build" {
-  triggers = {
-    always = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command     = "make build"
-    working_dir = path.module
-  }
-}
-
-resource "null_resource" "serverless_deploy" {
-  triggers = {
-    always = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command     = "serverless deploy --verbose"
-    working_dir = path.module
-  }
-
-  depends_on = [
-    local_file.serverless_config,
-    null_resource.make_build,
-  ]
-}
